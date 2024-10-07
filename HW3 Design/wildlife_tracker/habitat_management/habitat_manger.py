47fe7868c16a9e9881ca0825f9c99e7d1e859798
syd-stokes <sstokes2004@gmail.com>
2024-10-07 18:03:21 -0400

    added 'self' parameter to necessary methods

--- HW3 Design/wildlife_tracker/habitat_management/habitat_manger.py
+++ HW3 Design/wildlife_tracker/habitat_management/habitat_manger.py
@@ -8,23 +8,23 @@ class HabitatManager:
     def __init__(self) -> None:
         habitats: dict[int, Habitat] = {}
 
-    def create_habitat(habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
+    def create_habitat(self, habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
         pass
 
-    def get_habitat_by_id(habitat_id: int) -> Habitat:
+    def get_habitat_by_id(self, habitat_id: int) -> Habitat:
         pass
 
-    def assign_animals_to_habitat(habitat_id: int, animals: List[Animal]) -> None:
+    def assign_animals_to_habitat(self, habitat_id: int, animals: List[Animal]) -> None:
         pass
 
-    def get_habitats_by_geographic_area(geographic_area: str) -> List[Habitat]:
+    def get_habitats_by_geographic_area(self, geographic_area: str) -> List[Habitat]:
         pass
 
-    def get_habitats_by_size(size: int) -> List[Habitat]:
+    def get_habitats_by_size(self, size: int) -> List[Habitat]:
         pass
 
-    def get_habitats_by_type(environment_type: str) -> List[Habitat]:
+    def get_habitats_by_type(self, environment_type: str) -> List[Habitat]:
         pass
 
-    def remove_habitat(habitat_id: int) -> None:
+    def remove_habitat(self, habitat_id: int) -> None:
         pass
--- HW3 Design/wildlife_tracker/migration_tracking/migration_manager.py
+++ HW3 Design/wildlife_tracker/migration_tracking/migration_manager.py
@@ -10,47 +10,47 @@ class MigrationManager:
         migrations: Dict[int, Migration] = {} 
         paths: Dict[int, MigrationPath] = {}
     
-    def cancel_migration(migration_id: int) -> None:
+    def cancel_migration(self, migration_id: int) -> None:
         pass
 
-    def get_migration_by_id(migration_id: int) -> Migration:
+    def get_migration_by_id(self, migration_id: int) -> Migration:
         pass
 
-    def remove_migration_path(path_id: int) -> None:
+    def remove_migration_path(self, path_id: int) -> None:
         pass
 
-    def create_migration_path(species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
+    def create_migration_path(self, species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
         pass
 
-    def get_migrations() -> list[Migration]:
+    def get_migrations(self) -> list[Migration]:
         pass
 
-    def get_migrations_by_current_location(current_location: str) -> list[Migration]:
+    def get_migrations_by_current_location(self, current_location: str) -> list[Migration]:
         pass
 
-    def get_migrations_by_start_date(start_date: str) -> list[Migration]:
+    def get_migrations_by_start_date(self, start_date: str) -> list[Migration]:
         pass
 
-    def get_migrations_by_status(status: str) -> list[Migration]:
+    def get_migrations_by_status(self, status: str) -> list[Migration]:
         pass
 
-    def schedule_migration(migration_path: MigrationPath) -> None:
+    def schedule_migration(self, migration_path: MigrationPath) -> None:
         pass
 
-    def get_migrations_by_migration_path(migration_path_id: int) -> list[Migration]:
+    def get_migrations_by_migration_path(self, migration_path_id: int) -> list[Migration]:
         pass
 
-    def get_migration_path_by_id(path_id: int) -> MigrationPath:
+    def get_migration_path_by_id(self, path_id: int) -> MigrationPath:
         pass
 
-    def get_migration_paths() -> list[MigrationPath]:
+    def get_migration_paths(self) -> list[MigrationPath]:
         pass
 
-    def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
+    def get_migration_paths_by_destination(self, destination: Habitat) -> list[MigrationPath]:
         pass
 
-    def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
+    def get_migration_paths_by_species(self, species: str) -> list[MigrationPath]:
         pass
 
-    def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
+    def get_migration_paths_by_start_location(self, start_location: Habitat) -> list[MigrationPath]:
         pass
