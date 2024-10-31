2c2a1f8126929db6f3eb628a5fd4ec29c2779274
syd-stokes <sstokes2004@gmail.com>
2024-10-30 22:25:59 -0400

    started work on 'meal_max.model.battle_model' docstrings

--- docstrings_testing/meal_max/meal_max/models/battle_model.py
+++ docstrings_testing/meal_max/meal_max/models/battle_model.py
@@ -11,11 +11,34 @@ configure_logger(logger)
 
 
 class BattleModel:
+    """
+    A class to manage meal-based battles.
+
+    Attributes:
+        combatants (List[Meal]): The list of meals participating in a given battle.
+
+    """
 
     def __init__(self):
+        """
+        Initializes the BattleModel with an empty list of combatants.
+        """
         self.combatants: List[Meal] = []
 
+    ##################################################
+    # Battle Management Functions 
+    ##################################################
+
     def battle(self) -> str:
+        """
+        Executes a battle between two meals in the combatants list and determines a winner.
+
+        Returns:
+            str: The name of the winning meal.
+
+        Raises:
+            ValueError: If there are fewer than two combatants to start a battle.
+        """
         logger.info("Two meals enter, one meal leaves!")
 
         if len(self.combatants) < 2:
@@ -67,12 +90,17 @@ class BattleModel:
         self.combatants.remove(loser)
 
         return winner.meal
-
-    def clear_combatants(self):
-        logger.info("Clearing the combatants list.")
-        self.combatants.clear()
-
+    
     def get_battle_score(self, combatant: Meal) -> float:
+        """
+        Calculates the battle score of a given meal combatant based on its attributes.
+
+        Args:
+            combatant (Meal): The meal combatant whose battle score to retrieve.
+
+        Returns:
+            float: The calculated score of the combatant.
+        """
         difficulty_modifier = {"HIGH": 1, "MED": 2, "LOW": 3}
 
         # Log the calculation process
@@ -86,12 +114,38 @@ class BattleModel:
         logger.info("Battle score for %s: %.3f", combatant.meal, score)
 
         return score
+    
+    ##################################################
+    # Combatant Management Functions
+    ##################################################
+
+    def clear_combatants(self):
+        """
+        Clears all meals from the combatants list.
+        """
+        logger.info("Clearing the combatants list.")
+        self.combatants.clear()
 
     def get_combatants(self) -> List[Meal]:
+        """
+        Retrieves a list of all meals currently set as combatants.
+
+        Returns:
+            List[Meal]: A list of current meal combatants.
+        """
         logger.info("Retrieving current list of combatants.")
         return self.combatants
 
     def prep_combatant(self, combatant_data: Meal):
+        """
+        Prepares a meal as a combatant for the battle, up to a maximum of two combatants.
+
+        Args:
+            combatant_data (Meal): The meal to add as a combatant.
+
+        Raises:
+            ValueError: If the combatant list already has two combatants.
+        """
         if len(self.combatants) >= 2:
             logger.error("Attempted to add combatant '%s' but combatants list is full", combatant_data.meal)
             raise ValueError("Combatant list is full, cannot add more combatants.")
